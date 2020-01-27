5403ae49cc8fa820cc986adcc555a10c9e295812
Sean Budning <seanbud@users.noreply.github.com>
2020-01-27 15:31:54 -0500

    Update RotateObj_Baker.py

--- Scripts/RotateObj_Baker.py
+++ Scripts/RotateObj_Baker.py
@@ -14,23 +14,21 @@ def TakePhoto(n):
     cur_file_name += str(n);
     
     # Take Photo
-    file = os.path.join("C:/Users/Sean-PC/Desktop", cur_file_name)
+    file = os.path.join(directory, cur_file_name + ".png")
     bpy.context.scene.render.filepath = file
     bpy.ops.render.render(write_still = True)
 
 
 
 # Main
-frames = 6
+frames = 36
 angle = 360/frames
-file_name = "golden_scrap"
+file_name = "health_cube_rotate"
+directory = os.path.dirname(bpy.data.filepath)
+
 
 for n in range(0, frames):
-
-    # Rotate all selected objects on z by angle
-    for obj in bpy.context.selected_objects:
-        obj.rotation_euler[2] += angle
-        
+    bpy.ops.transform.rotate(value=(angle * 2 * math.pi / 360.0))
     TakePhoto(n)
     
 print("\nCompleted.")
